df6d5ecd1bd0f248b2eafb76c1a6cef47e3e415a
anhnvq214947 <136361166+anhnvq214947@users.noreply.github.com>
2023-06-15 04:58:26 +0700

    Update scan system.java

--- scan system.java
+++ scan system.java
@@ -1,37 +1,35 @@
-import org.clamav4j.ClamScan;
-import org.clamav4j.ClamScanOptions;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.io.File;
+public class ClamAVScanner {
+    /**
+     * This function scans the system using ClamAV antivirus
+     *
+     * @return String: The result of the scan
+     */
+    public static String clamAVScanSystem() {
+        try {
+            // Run the ClamAV scan command
+            Process process = Runtime.getRuntime().exec("clamscan -r /");
 
-public class ClamAVExample {
-    public static void main(String[] args) throws Exception {
-        // Khởi tạo ClamScan object
-        ClamScan clam = new ClamScan();
-
-        // Tạo đối tượng ClamScanOptions để thiết lập các tùy chọn quét virus
-        ClamScanOptions options = new ClamScanOptions();
-        options.setFilename("/path/to/clamd.sock");  // Đặt đường dẫn đến socket của ClamAV
-
-        // Điều hướng đến thư mục gốc của hệ thống
-        File rootDirectory = File.listRoots()[0];
-
-        // Gọi phương thức quét virus cho từng file trên hệ thống
-        scan(rootDirectory, clam, options);
-    }
-
-    private static void scan(File file, ClamScan clam, ClamScanOptions options) throws Exception {
-        if (file.isDirectory()) {
-            // Nếu là thư mục, duyệt các file con bên trong
-            File[] files = file.listFiles();
-            for (File child : files) {
-                scan(child, clam, options);
+            // Read the output of the command
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
             }
-        } else {
-            // Nếu là file, quét virus và hiển thị kết quả nếu có virus
-            boolean isInfected = clam.scan(file, options);
-            if (isInfected) {
-                System.out.println("Virus detected in file: " + file.getAbsolutePath());
-            }
+
+            // Wait for the process to finish
+            int exitVal = process.waitFor();
+
+            // Return the output of the command
+            return output.toString();
+        } catch (IOException | InterruptedException e) {
+            // Log the error
+            System.err.println("Error: " + e.getMessage());
+            return "";
         }
     }
 }
