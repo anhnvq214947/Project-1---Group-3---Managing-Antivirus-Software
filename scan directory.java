30f5870b02807504622eebd4be83d89a9a24d193
anhnvq214947 <136361166+anhnvq214947@users.noreply.github.com>
2023-06-15 05:25:47 +0700

    Update scan directory.java

--- scan directory.java
+++ scan directory.java
@@ -1,31 +1,19 @@
-import org.clamav4j.ClamScan;
-import org.clamav4j.ClamScanOptions;
+import java.io.File;
+import java.util.List;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.clamav4j.*;
 
 public class ClamAVExample {
     public static void main(String[] args) throws Exception {
-        // Khởi tạo ClamScan object
-        ClamScan clam = new ClamScan();
+        String dirPath = "/path/to/my/directory";
 
-        // Tạo đối tượng ClamScanOptions để thiết lập các tùy chọn quét virus
-        ClamScanOptions options = new ClamScanOptions();
-        options.setFilename("/path/to/clamd.sock");  // Đặt đường dẫn đến socket của ClamAV
+        // Create a new ClamScan instance
+        ClamScan clamScan = new ClamScan();
 
-        // Thiết lập thư mục cần quét
-        Path directoryToScan = Paths.get("/path/to/directory");
+        // Set the path to the ClamAV daemon and virus definition database
+        clamScan.setHost("localhost");
+        clamScan.setPort(3310);
+        clamScan.setTimeout(60000);
+        clamScan.setMaxChunkSize(262144);
 
-        // Lấy danh sách các file trong thư mục
-        Files.walk(directoryToScan)
-                .filter(path -> !Files.isDirectory(path))  // Loại bỏ các thư mục con
-                .forEach(path -> {
-                    // Quét từng file và hiển thị kết quả
-                    boolean isInfected = clam.scan(path.toFile(), options);
-                    if (isInfected) {
-                        System.out.println("Virus detected in file: " + path.toString());
-                    }
-                });
-    }
-}
+        // Get a list of files
